c651bb4ad6b5b08bead12609084f6a28fbcfe753
barshaul <barshaul@amazon.com>
2024-08-08 15:26:33 +0000

    Added pubusb with other client to the demo

--- demo/pubsub_demo_sync/pubsub_demo_sync.py
+++ demo/pubsub_demo_sync/pubsub_demo_sync.py
@@ -1,26 +1,13 @@
 import argparse
-import asyncio
 import os
+import time
 from collections import deque
 from datetime import datetime, timezone
 
-from glide import (
-    ByAddressRoute,
-    GlideClientConfiguration,
-    GlideClusterClient,
-    GlideClusterClientConfiguration,
-    Logger,
-    LogLevel,
-    NodeAddress,
-    PubSubMsg,
-    SlotKeyRoute,
-    SlotType,
-)
+import redis
 from quart import Quart, jsonify, render_template, request
+from redis.cluster import RedisCluster
 
-Logger.set_logger_config(LogLevel.INFO)
-PubSubChannelModes = GlideClusterClientConfiguration.PubSubChannelModes
-PubSubSubscriptions = GlideClusterClientConfiguration.PubSubSubscriptions
 app = Quart(__name__)
 
 publisher_messages = deque()
@@ -29,46 +16,42 @@ cluster_info = {}
 
 publisher = None
 listener = None
-channel = "f111"
+global_client = None
+channel = "key4"
 
 
-async def create_pubsub_clients():
+def create_pubsub_clients():
     global channel
-    cluster_channels_and_patterns = {PubSubChannelModes.Exact: {channel}}
-    pubsub_subscriptions = PubSubSubscriptions(
-        channels_and_patterns=cluster_channels_and_patterns, callback=None, context=None
-    )
-    config = GlideClusterClientConfiguration(
-        [NodeAddress(host="localhost", port=app.config["VALKEY_PORT"])],
-        use_tls=False,
-        pubsub_subscriptions=pubsub_subscriptions,
-    )
-    global publisher
+    global global_client
     global listener
-    publisher = await GlideClusterClient.create(config)
-    listener = await GlideClusterClient.create(config)
+    global publisher
+    port = app.config["VALKEY_PORT"]
+    global_client = RedisCluster(host="localhost", port=port, decode_responses=True)
+    publisher = RedisCluster(host="localhost", port=port)
+    listener = RedisCluster(host="localhost", port=port, decode_responses=True).pubsub()
+    listener.subscribe(channel)
     return publisher, listener
 
 
-async def publish_messages():
+def publish_messages():
     global channel
     while True:
         try:
             timestamp = datetime.now(timezone.utc).strftime("%H:%M:%S")
             message = f"hello_{timestamp}"
-            await publisher.publish(message, channel)
+            publisher.publish(channel, message)
             publisher_messages.appendleft(message)
         except Exception as e:
-            print(f"Publisher received error {e}")
-        await asyncio.sleep(1)
+            print(f"Publisher eceived error {e}")
+        time.sleep(1)
 
 
-async def listen_messages():
+def listen_messages():
     while True:
         try:
-            msg = await listener.get_pubsub_message()
+            msg = listener.get_message(ignore_subscribe_messages=True, timeout=None)
             if msg:
-                message = msg.message.decode()
+                message = msg["data"]
                 listener_messages.appendleft(message)
         except Exception as e:
             print(f"Listener received error {e}")
@@ -76,7 +59,7 @@ async def listen_messages():
 
 @app.route("/")
 async def index():
-    return await render_template("index.html", client_type="Valkey GLIDE")
+    return await render_template("index.html", client_type="Other")
 
 
 @app.route("/publisher_messages")
@@ -91,77 +74,68 @@ async def get_listener_messages():
 
 @app.route("/cluster_nodes")
 async def cluster_nodes():
-    nodes = await get_cluster_nodes()
+    nodes = get_cluster_nodes()
     return jsonify({"nodes": nodes})
 
 
 @app.route("/channel_slot")
 async def channel_slot():
     global channel
-    slot = await get_channel_slot(channel)
+    slot = get_channel_slot(channel)
     return jsonify({"slot": slot})
 
 
-async def get_cluster_nodes():
-    nodes = await publisher.custom_command(
-        ["CLUSTER", "NODES"],
-        route=ByAddressRoute("127.0.0.1", app.config["VALKEY_PORT"]),
-    )
-    nodes = nodes.decode("utf-8")
+def get_cluster_nodes():
+    nodes = global_client.execute_command("CLUSTER NODES")
     parsed_nodes = parse_cluster_nodes(nodes)
     return parsed_nodes
 
 
-async def get_channel_slot(channel):
-    slot = await publisher.custom_command(
-        ["CLUSTER", "KEYSLOT", channel],
-        route=ByAddressRoute("127.0.0.1", app.config["VALKEY_PORT"]),
-    )
+def get_channel_slot(channel):
+    slot = global_client.execute_command("CLUSTER KEYSLOT", channel)
     return slot
 
 
-def parse_cluster_nodes(nodes_str):
-    nodes = nodes_str.strip().split("\n")
+def parse_cluster_nodes(nodes_dict):
     shards = {}
     failed_nodes = []
 
-    for node in nodes:
-        parts = node.split()
-        node_id = parts[0]
-        ip_port = parts[1].split("@")[0]  # Remove the @suffix
-        flags = parts[2].split(",")
-        master_id = parts[3]
-        slots = parts[8] if len(parts) > 8 else ""
+    for ip_port, node_info in nodes_dict.items():
+        node_id = node_info["node_id"]
+        flags = node_info["flags"].split(",")
+        master_id = node_info["master_id"]
+        slots = node_info["slots"]
 
         shard_id = master_id if master_id != "-" else node_id
         if shard_id not in shards:
             shards[shard_id] = {"slots": slots, "primary": "", "replicas": []}
 
-        health = "healthy" if "fail" not in flags else "failed"
-        if "master" in flags:
+        health = "healthy" if node_info["connected"] else "failed"
+        if "master" in flags or "myself,master" in flags:
             if health == "failed":
-                failed_nodes.append(
-                    f'<span class="failed">primary {ip_port} {health}</span>'
-                )
+                failed_nodes.append(f'<span class="failed">{ip_port} {health}</span>')
             else:
                 shards[shard_id]["primary"] = f"{ip_port} {health}"
                 shards[shard_id]["slots"] = slots
         else:
             shards[shard_id]["replicas"].append(
-                f'<span class="replica">replica {ip_port} {health}</span>'
+                f'<span class="replica">{ip_port} {health}</span>'
             )
 
     result = []
     for idx, (shard_id, shard_info) in enumerate(shards.items(), start=1):
         if shard_info["primary"] and "healthy" in shard_info["primary"]:
-            result.append(
-                f'<strong>---<br>Shard {idx} Slots {shard_info["slots"]}</strong>'
+            slots = (
+                ", ".join([f"{slot[0]}-{slot[1]}" for slot in shard_info["slots"]])
+                if shard_info["slots"]
+                else "None"
             )
+            result.append(f"<strong>---<br>Shard {idx} Slots {slots}</strong>")
             result.append(
                 f'<span class="primary">primary {shard_info["primary"]}</span>'
             )
             for replica in shard_info["replicas"]:
-                result.append(f"{replica}")
+                result.append(f'<span class="replica">replica {replica}</span>')
 
     if failed_nodes:
         result.append("<strong>---<br>Failed Nodes</strong>")
@@ -204,15 +178,16 @@ async def kill_primary_node():
 
 @app.before_serving
 async def before_serving():
-    await create_pubsub_clients()
-    asyncio.create_task(publish_messages())
-    asyncio.create_task(listen_messages())
+    create_pubsub_clients()
+    app.add_background_task(publish_messages)
+    app.add_background_task(listen_messages)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Valkey Client Demo")
     parser.add_argument("--port", type=int, default=20695, help="Valkey server port")
+    parser.add_argument("--client", type=str, default="Other", help="The client type")
     args = parser.parse_args()
     app.config["VALKEY_PORT"] = args.port
-    app.config["CLIENT_TYPE"] = "Valkey GLIDE"
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.config["CLIENT_TYPE"] = args.client
+    app.run(debug=True, port=4000, host="0.0.0.0")
